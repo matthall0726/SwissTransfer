226eb2d8d4409d26ec7d57b2d892b27471e82461
matthall0726 <matt.hall0726@gmail.com>
2022-12-20 21:40:04 -0500

    Update MacOS Login

--- Swiss Transfer/CTk Frame/main.py
+++ Swiss Transfer/CTk Frame/main.py
@@ -1,41 +1,57 @@
-import time
-from tkinter import *
 import os
-from applescript import tell
-from PIL import Image, ImageTk
-import customtkinter
+import platform
 import webbrowser
+from tkinter import messagebox
+from tkinter import *
+import customtkinter
+from PIL import Image
+
 
 class App(customtkinter.CTk):
+    """Container for the Swiss Application"""
+
     def __init__(self):
-        super().__init__()
-        customtkinter.set_appearance_mode("system")
-
-
-
-
-        self.title("")
-
+        customtkinter.CTk.__init__(self)
+        self.title("Swiss Transfer")
         self.geometry("950x750")
-        self.resizable(False,False)
         self.minsize(400, 300)
-
-        #create a grid layout
+        self.resizable(False, False)
+        container = customtkinter.CTkFrame(self)
+        container.pack(side="top", fill="both", expand=True)
+        # create a grid layout
         self.grid_rowconfigure(5, weight=1)
         self.grid_columnconfigure(7, weight=0)
+        self.frames = {}
+
+        for page in (LandingPage, HomePage):
+            frame = page(container, self)
+            self.frames[page] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(LandingPage)
+
+    def show_frame(self, content):
+        frame = self.frames[content]
+        frame.tkraise()
 
 
+class LandingPage(customtkinter.CTkFrame):
+    """Login Page for the Swiss Transfer Application"""
 
-        productName_label = customtkinter.CTkLabel(master=self, text="Swiss Transfer ®")
-        productName_label.grid(row=0,column=7,pady=10)
+    def __init__(self, parent, controller): # TODO implement controller for switching to the home page
+        customtkinter.CTkFrame.__init__(self, parent)
+        customtkinter.set_appearance_mode("system")
+
+        product_name_label = customtkinter.CTkLabel(master=self, text="Swiss Transfer ®")
+        product_name_label.grid(row=0, column=7, pady=10)
 
         space_label = customtkinter.CTkLabel(master=self, text="")
-        space_label.grid(row=1,column=0,padx=25)
+        space_label.grid(row=1, column=0, padx=25)
 
         label_username = customtkinter.CTkLabel(master=self, text="Username")
-        label_username.grid(row=1, column=1, padx=0,pady=15, sticky="n")
+        label_username.grid(row=1, column=1, padx=0, pady=15, sticky="n")
 
-        textbox_username = customtkinter.CTkEntry(self, width=100,height=5)
+        textbox_username = customtkinter.CTkEntry(self, width=100, height=5)
         textbox_username.grid(row=1, column=2, sticky="n", padx=5, pady=20)
 
         label_password = customtkinter.CTkLabel(master=self, text="Password", )
@@ -46,36 +62,61 @@ class App(customtkinter.CTk):
         label_password = customtkinter.CTkLabel(master=self, text="Domain")
         label_password.grid(row=1, column=5, padx=5, pady=15, sticky="n")
 
-        textbox_password = customtkinter.CTkEntry(self, width=100, height=5)
-        textbox_password.grid(row=1, column=6, sticky="n", padx=5, pady=20)
+        # need to change domain name to something else @Matthew
+        textbox_domain = customtkinter.CTkEntry(self, width=100, height=5)
+        textbox_domain.grid(row=1, column=6, sticky="n", padx=5, pady=20)
 
-        def loginbutton():
-            print("Button Pressed")
-            terminal_command = """ osascript -e '
-            
-            tell application "Terminal"
-                reopen
-                activate
-                do script "ssh mhall087@swisstransfer.net" in front window
-                delay 2
-                do script "Mrh0726!." in front window
-            end tell
-            
-            '"""
+        def login_button():
+            """ This function is responsible for logging into the SSH Server using username and password fields"""
 
-            os.system(terminal_command)
+            system_info = platform.system().lower()
+            uname = textbox_username.get()
+            password = textbox_password.get()
+            domain = textbox_domain.get()
+
+            if system_info.__contains__("linux"):  # Detects if the user is running the application from linux
+                terminal_command = "sshpass -p \"{0}\" ssh -o StrictHostKeyChecking=no {1}@swisstransfer.net"
+                terminal_command = terminal_command.format(password, uname)
+                os.system(terminal_command)
+                # TODO implement a way to check if the user has been authenticated then destroy the landing page will
+                #  be destroyed -> self.destroy()
+
+            elif system_info.__contains__("darwin"):  # TODO needs to correctly detect os x (mac)
+                terminal_command = """ osascript -e '
+                        tell application "Terminal"
+                        reopen
+                        activate
+                        do script "ssh {0}@{1}" in front window
+                        delay 2
+                        do script "{2}" in front window
+                        end tell
+                        '"""
+                if domain == "":
+                    domain = "swisstransfer.net"
+
+                if uname != "" and password != "":
+                    terminal_command = terminal_command.format(uname, domain, password)
+                    os.system(terminal_command)
+                else:
+                    messagebox.showerror("Error", "Please enter a username and password, and then try again.")
 
 
+            elif system_info.__contains__("win"): # FIXME create windows script for openssh and login
+                print("windows feature is not enabled")
+                open_ssh_install = "Get-WindowsCapability -Online -Name OpenSSH.Client* | Add-WindowsCapability -Online"
+                os.system(open_ssh_install)
+
+            else:
+                messagebox.showerror("Error","The current version of your operating system could not be detected.")
 
 
-
-
-
-        login_button = customtkinter.CTkButton(master=self,
-                                               text="Login",fg_color="grey",
-                                               text_color="white",
-                                               hover_color="#404040",
-                                               command=loginbutton)
+        login_button = customtkinter.CTkButton(
+            master=self,
+            text="Login", fg_color="grey",
+            text_color="white",
+            hover_color="#404040",
+            command=login_button
+        )
         login_button.grid(row=1, column=7, sticky="n", padx=10, pady=18)
 
         # Login Image
@@ -83,16 +124,22 @@ class App(customtkinter.CTk):
         # appends it to the button where
         # it gets filled to the size of the
         # image
-        my_image = customtkinter.CTkImage(light_image=Image.open("CTk Frame/Transfer Photo-850x330.jpeg"),
-                                          dark_image=Image.open("CTk Frame/Transfer Photo-850x330.jpeg"),
-                                          size=(800, 330))
 
-        login_image = customtkinter.CTkButton(master=self,
-                                              image=my_image,
-                                              text="",hover=False,
-                                              fg_color="#404040")
+        # my_image = customtkinter.CTkImage(light_image=Image.open("CTk Frame/Transfer Photo-850x330.jpeg"),
+        #                                   dark_image=Image.open("CTk Frame/Transfer Photo-850x330.jpeg"),
+        #                                   size=(800, 330))
 
-        login_image.grid(row=2,column=1,columnspan=7)
+        # For Michael
+        my_image = customtkinter.CTkImage(
+            light_image=Image.open(
+                "/Users/matthall/Desktop/Personal Projects/GitHub Repo - Swiss Transfer/SwissTransfer/Swiss Transfer/CTk Frame/Transfer Photo-850x330.jpeg"),
+            dark_image=Image.open(
+                "/Users/matthall/Desktop/Personal Projects/GitHub Repo - Swiss Transfer/SwissTransfer/Swiss Transfer/CTk Frame/Transfer Photo-850x330.jpeg"
+            ),
+            size=(800, 330))
+
+        login_image = customtkinter.CTkButton(master=self, image=my_image, text="", hover=False, fg_color="#404040")
+        login_image.grid(row=2, column=1, columnspan=7)
 
         # Help Label
         # Redirects user from client
@@ -100,17 +147,22 @@ class App(customtkinter.CTk):
         # how ssh works
 
         url = "https://www.a2hosting.com/kb/getting-started-guide/accessing-your-account/using-ssh-secure-shell"
-        def OpenUrl(url):
-            webbrowser.open(url)
-            print("button pressed")
-        help_button_spacing = customtkinter.CTkLabel(master=self, text="")
-        #help_button_spacing.grid(row=3,column=0,padx=25)
 
-        help_button = customtkinter.CTkButton(master=self, text="Help",command=lambda aurl=url:OpenUrl(aurl))
+        def open_url(this_url):
+            webbrowser.open(this_url)
+            print("button pressed")
+
+        help_button_spacing = customtkinter.CTkLabel(master=self, text="")
+        help_button_spacing.grid(row=3, column=0, padx=25)
+        help_button = customtkinter.CTkButton(master=self, text="Help", command=lambda a_url=url: open_url(a_url))
         help_button.grid(row=3, column=7, pady=15)
 
 
+class HomePage(customtkinter.CTkFrame):
+    """This page purpose is to allow users to send and receive files on their system"""
 
+    def __init__(self, parent, controller):
+        customtkinter.CTkFrame.__init__(self, parent)
 
 
 if __name__ == "__main__":
